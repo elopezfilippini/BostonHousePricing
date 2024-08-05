a556771187c82e23d9dae88b896f14e0e678e1f7
elopezfilippini <elopezfilippini@gmail.com>
2024-08-05 18:41:41 -0300

    cambios en app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import numpy as np
 import pandas as pd
 import pickle
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template ("home.html"
+    return render_template ("home.html")
 
 # Cargar el modelo previamente guardado
 with open('regmodel.pkl', 'rb') as file:
